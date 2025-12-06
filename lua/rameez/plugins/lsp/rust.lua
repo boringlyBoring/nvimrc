2e3fbf96a1b943482b6bf608b386dde8bbbd3984
Mohd Rameez <rameezmohd739@gmail.com>
2025-12-06 19:21:31 +0530

    Add rust plug in and install rust lsp

--- lua/rameez/plugins/lsp/rust.lua
+++ lua/rameez/plugins/lsp/rust.lua
@@ -1,37 +1,36 @@
 -- ~/.config/nvim/lua/plugins/rust.lua
 return {
-  {
-    "simrat39/rust-tools.nvim",
-    ft = "rust",
-    dependencies = {
-      "nvim-lspconfig",
-      "mfussenegger/nvim-dap", -- Optional, for debugging
-    },
-    opts = function()
-      local rust_tools_opts = {}
-      -- Configure rust-tools.nvim options here
-      -- Example:
-      -- rust_tools_opts.tools = {
-      --   autoSetHints = true,
-      --   inlay_hints = {
-      --     show_parameter_hints = true,
-      --     parameter_hints_prefix = "<- ",
-      --     metrics_hint_prefix = "-> ",
-      --   },
-      -- }
-      return rust_tools_opts
-    end,
-    config = function(_, opts)
-      require("rust-tools").setup(opts)
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    ft = "rust",
-    opts = {
-      servers = {
-        rust_analyzer = {}, -- This enables rust-analyzer LSP
-      },
-    },
-  },
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+		config = function()
+			require("mason").setup()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "rust_analyzer" },
+			})
+			require("lspconfig").rust_analyzer.setup({
+				settings = {
+					["rust-analyzer"] = {
+						imports = {
+							granularity = {
+								group = "module",
+							},
+							prefix = "self",
+						},
+						cargo = {
+							buildScripts = {
+								enable = true,
+							},
+							procMacro = {
+								enable = true,
+							},
+						},
+					},
+				},
+			})
+		end,
+	},
 }
---
+++ lua/rameez/plugins/lsp/rustaceanvim.lua
@@ -0,0 +1,9 @@
+-- lua/plugins/rust.lua
+
+return {
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^4", -- Recommended: Use the latest stable version
+		ft = { "rust" },
+	},
+}
